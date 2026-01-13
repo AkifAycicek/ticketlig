26fd75998777321da49ed4e5493371f871f07351
AkifAycicek <akifaycicek@gmail.com>
2026-01-13 18:41:56 +0300

    feat(error-handling): Add error handling with i18n and toast notifications
    - Implement error handling with i18n support for localized messages
    - Display a warning toast notification for errors
    - Construct summary and detail texts using i18n translations or fallback keys
    - Use nextTick to ensure UI updates after error logging

---
+++ src/lib/appErrorHandler.ts
@@ -0,0 +1,30 @@
+import { app } from '@/main';
+
+export const appErrorHandler = async (error: any) => {
+   if (!error.status && !error.code) return;
+
+   console.error(error);
+
+   await nextTick(async () => {
+      const toast = app.config?.globalProperties?.$toast;
+
+      const summaryKey = error.name || error.status;
+      const summaryPath = 'errors.' + summaryKey;
+      const summaryText = i18n.te(summaryPath) ? i18n.t(summaryPath) : summaryKey || '';
+
+      const detailKey =
+         error.message || error.details || error.code || error.description || error.hint;
+      const detailPath = 'errors.' + detailKey;
+      const detailText = i18n.te(detailPath) ? i18n.t(detailPath) : detailKey || '';
+
+      const summary = summaryText;
+      const detail = detailText;
+
+      toast?.add?.({
+         life: 0,
+         severity: ToastSeverity.WARN,
+         summary,
+         detail
+      });
+   });
+};
