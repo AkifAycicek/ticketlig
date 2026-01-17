72e0494a6a33ff4968047a4e5e020cd209b9c5d3
AkifAycicek <akifaycicek@gmail.com>
2026-01-17 18:18:04 +0300

    refactor(http): make response data optional in http methods
    Update all HTTP methods to use optional data type in response objects, allowing for cases where data may not be present. This change improves flexibility by accommodating APIs that do not always return data payloads.

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -4,34 +4,34 @@ import { AxiosInstance } from 'axios';
 interface Http {}
 
 class Http {
-   client: AxiosInstance = createAxiosClient();
+   protected client: AxiosInstance = createAxiosClient();
 
    protected options<T>(url: string, options?: Headers) {
-      return this.client.request<{ data: T }>({ method: 'OPTIONS', url, ...options });
+      return this.client.request<{ data?: T }>({ method: 'OPTIONS', url, ...options });
    }
 
    protected head<T>(url: string, options?: Headers) {
-      return this.client.request<{ data: T }>({ method: 'HEAD', url, ...options });
+      return this.client.request<{ data?: T }>({ method: 'HEAD', url, ...options });
    }
 
    protected get<T>(url: string, options?: Headers) {
-      return this.client.request<{ data: T }>({ method: 'GET', url, ...options });
+      return this.client.request<{ data?: T }>({ method: 'GET', url, ...options });
    }
 
    protected delete<T>(url: string, options?: Headers) {
-      return this.client.request<{ data: T }>({ method: 'DELETE', url, ...options });
+      return this.client.request<{ data?: T }>({ method: 'DELETE', url, ...options });
    }
 
-   protected post<T>(url: string, data: unknown, options?: Headers) {
-      return this.client.request<{ data: T }>({ method: 'POST', url, data, ...options });
+   protected post<T>(url: string, data: T | unknown, options?: Headers) {
+      return this.client.request<{ data?: T }>({ method: 'POST', url, data, ...options });
    }
 
    protected put<T>(url: string, data: unknown, options?: Headers) {
-      return this.client.request<{ data: T }>({ method: 'PUT', url, data, ...options });
+      return this.client.request<{ data?: T }>({ method: 'PUT', url, data, ...options });
    }
 
    protected patch<T>(url: string, data: unknown, options?: Headers) {
-      return this.client.request<{ data: T }>({ method: 'PATCH', url, data, ...options });
+      return this.client.request<{ data?: T }>({ method: 'PATCH', url, data, ...options });
    }
 }
 
